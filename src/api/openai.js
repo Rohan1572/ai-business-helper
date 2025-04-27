9c258aed20811712fd73ce9db920ef8c72144893
Rohan Gupta <hellorohan02@gmail.com>
2025-04-27 22:57:06 +0530

    first commit

--- src/api/openai.js
+++ src/api/openai.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// const API_KEY = "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";
+
 
 const openaiInstance = axios.create({
   baseURL: "https://api.openai.com/v1/chat/completions",
